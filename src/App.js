b30698032017f29dfa5bbafaceb270d800d17891
Ross Ferreira <ross.p.ferreira@gmail.com>
2020-03-30 23:21:34 +0100

    Adjusted App to use Router/Routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { HashRouter as Router, Route, } from "react-router-dom";
+
 import Value from "./components/Value"; 
 import Buttons from "./components/Buttons";
 
 
 
 
-const App = ({count,handleIncrement,handleDecrement,handleReset,value}) => ( 
+const App = () => ( 
   <React.Fragment>
-    <h1>iCounter</h1>
-    <Value/>
-    <Buttons
-    />
+    <Router>
+      <Route exact path="/">
+        <h1>iCounter</h1>
+        <Value/>
+        <Buttons/>
+      </Route>
+    </Router>
   </React.Fragment>
 );
 export default App;
+
+
+
 
