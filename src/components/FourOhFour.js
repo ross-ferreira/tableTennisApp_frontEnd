2330800fb9b93441bba65cab499370e0cf6b72fd
Ross Ferreira <ross.p.ferreira@gmail.com>
2020-04-06 12:04:53 +0100

    added new routes for 404 page images

--- src/components/FourOhFour.js
+++ src/components/FourOhFour.js
@@ -1,14 +1,21 @@
 import React from "react";
 
+import pug from '../images/pug_on_horse.jpeg'
+
 const FourOhFour = () => {
 
     return (
         <>
-            
-            <picture>
-            <img src="https://lh3.googleusercontent.com/proxy/tZgiKfoJ2zh8hphvDuVrXb0RH23YqcFTmUzjn4QJWvB6zJY0TZeX-naj1byA05ac5Qx7jXP-PtAuNZ-uGzF8tMUFmjeykURUQzM1X3NlzEi6X0BBPsBCP8HAz2GIN4vdWMQjcZmyZ52bchtfHFTlGzQWammd_SCBe5cHYA"/>
+            <picture className="fOf">
+                <h3>HOWDY PARTNER!!</h3>
+                <img 
+                style={{width: 250, height: 250 }} 
+                resizeMode="contain"
+                src={pug} alt="pug"/>
+                <h3>Wasnt expecting to see YOU Here</h3>
+                <h3>!404!</h3>
             </picture>
-            <h3>Wasnt expecting to see YOU Here</h3>
+            
         </>
     )
 }
